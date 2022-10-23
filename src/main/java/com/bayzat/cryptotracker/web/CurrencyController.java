fea147f977535569935d4c7c3ffbb68980956852
ko4evneg <ko4evneg@gmail.com>
2022-10-23 15:48:34 +0300

    - Add roles access control to CurrencyController

--- src/main/java/com/bayzat/cryptotracker/web/CurrencyController.java
+++ src/main/java/com/bayzat/cryptotracker/web/CurrencyController.java
@@ -4,6 +4,10 @@ import com.bayzat.cryptotracker.model.Currency;
 import com.bayzat.cryptotracker.model.to.CurrencyTo;
 import com.bayzat.cryptotracker.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +22,30 @@ public class CurrencyController extends EntityController<Currency, CurrencyTo> {
     @PostConstruct
     public void init() {
         service = currencyService;
+    }
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> save(@RequestBody CurrencyTo to, @PathVariable Long id) {
+        return super.save(to, id);
+    }
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> saveNew(@RequestBody CurrencyTo to) {
+        return super.saveNew(to);
+    }
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return super.delete(id);
+    }
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> update(@RequestBody CurrencyTo to, @PathVariable Long id) {
+        return super.update(to, id);
     }
 }
 
